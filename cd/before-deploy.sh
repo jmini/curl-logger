a13217c4c3e6b6153399b502235cdd9f98ce6fdc
Maciej Gawinecki <mgawinecki@gmail.com>
2017-07-24 01:02:36 +0200

    Fix encryption problems

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_6b751c30f51d_key -iv $encrypted_6b751c30f51d_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_6b751c30f51d_key -iv $encrypted_6b751c30f51d_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
