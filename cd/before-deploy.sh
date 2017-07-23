23f8d58c301dd97c8dde6854762a9d96fb0774f2
Maciej Gawinecki <mgawinecki@gmail.com>
2017-07-24 00:33:10 +0200

    Log instructions in bash scripts

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+set -x
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_6b751c30f51d_key -iv $encrypted_6b751c30f51d_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import cd/signingkey.asc
+    gpg --fast-import cd/codesigning.asc
 fi
--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
 fi
