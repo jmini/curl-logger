9f6e4e239855ac6d62e70c100675bdc0fba1bab2
Maciej Gawinecki <mgawinecki@gmail.com>
2017-09-01 07:26:45 +0200

    Configure Travis to decrypt secrets only in release branch

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'release' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_0705285cb685_key -iv $encrypted_0705285cb685_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
