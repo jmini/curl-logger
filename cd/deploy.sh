592d7302f2ac810b4b088d09876c3175051f9ac0
Maciej Gawinecki <mgawinecki@gmail.com>
2017-09-01 07:22:55 +0200

    Configure Travis to release to Maven Central only from release branch

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'release' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy  -P release --settings cd/mvnsettings.xml
 fi
